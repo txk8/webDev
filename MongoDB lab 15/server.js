b81ce56eeebc9cd4e38a84f080425077c9ddca93
txk8 <kitcat1570@googlemail.com>
2018-04-24 13:10:01 +0100

    server js

---
+++ MongoDB lab 15/server.js
@@ -0,0 +1,25 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/star_wars_quotes";
+const express = require('express');
+const app = express();
+app.use(express.static('public'))
+var db;
+MongoClient.connect(url, function(err, database){
+ if(err) throw err;
+ db = database;
+ app.listen(8080);
+});
+
+app.get('/all', function(req, res) {
+ db.collection('quotes').find().toArray(function(err, result) {
+ if (err) throw err;
+ var output = "<h1>All the quotes</h1>";
+ for (var i = 0; i < result.length; i++) {
+ output += "<div>"
+ output += "<h3>" + result[i].name + "</h3>"
+ output += "<p>" + result[i].quote + "</p>"
+ output += "</div>"
+ }
+ res.send(output);
+ });
+});
